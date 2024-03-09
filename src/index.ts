f54ffc8569fa66b30567ed025bae9ecc4008489d
Evan Henry <ehenry344@gmail.com>
2024-03-09 00:24:22 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import "dotenv/config" 
+import {Client, GatewayIntentBits, Events, TextChannel} from "discord.js" 
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+})
+
+client.on(Events.ClientReady, async () => {
+    console.log("Client Ready"); 
+}); 
+
+async function main() { 
+    return client.login(process.env.BOT_TOKEN); 
+}
+
+main()
+.then(() => {
+    client.channels.fetch("1050887114335723520").then((guildChannel) => {
+        if (guildChannel && guildChannel.isTextBased()) { 
+            guildChannel.send("The notion that I am a terrible professor is severly mistaken"); 
+        }
+    }); 
+})
+.catch((failurePathology) => { 
+    console.error(failurePathology); 
+}); 
