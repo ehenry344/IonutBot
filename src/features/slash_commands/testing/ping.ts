7b057252ab55fa5a99ff8410e6eba40603f1d842
Evan Henry <ehenry344@gmail.com>
2024-03-12 21:44:00 -0400

    axios dependency added

--- src/features/slash_commands/testing/ping.ts
+++ src/features/slash_commands/testing/ping.ts
@@ -6,13 +6,13 @@ import {
 } from 'discord.js'
 
 export default {
+    interactionName: 'ping', 
     registrationData: <ApplicationCommandData>{
         name: 'ping',
         description: 'slash command system check',
         type: ApplicationCommandType.ChatInput,
         dmPermission: false 
     },
-    
     callback: async (interaction: ChatInputCommandInteraction) => {
         interaction.reply("pong"); 
     }
--- src/services/interaction_service.ts
+++ src/services/interaction_service.ts
@@ -3,8 +3,6 @@
 // Created: 3 / 9 / 2024
 // Updated: 3 / 12 / 2024
 
-
-
 export class InteractionService {
     public static async RegisterInteractions() {
 
