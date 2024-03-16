7097404f2558ec3ccad43f3dfb8d73932cc8402d
Evan Henry <ehenry344@gmail.com>
2024-03-16 14:07:36 -0400

    Mini update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+// prettier-ignore 
 import {
     Client,
     GatewayIntentBits,
--- src/services/interaction_service.ts
+++ src/services/interaction_service.ts
@@ -3,8 +3,17 @@
 // Created: 3 / 9 / 2024
 // Updated: 3 / 12 / 2024
 
-export class InteractionService {
-    public static async RegisterInteractions() {
+import "axios" 
 
+export class InteractionService {
+    
+    public static async RegisterInteractions() {
+        const commandFile = "./features/slash_commands/testing/ping.js" 
+
+        const commandInstance = import(commandFile)
+
+        // just testing command registration for the bot 
+
+        
     }
 }
